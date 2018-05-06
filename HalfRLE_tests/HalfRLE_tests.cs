145c817b5d40c81d753077b1708ddc91e9322dfd
Sergey/ka <acelightnin9@gmail.com>
2018-05-06 13:41:11 +0300

    Added Unit Test project and unit test for GetBytes method

---
+++ HalfRLE_tests/HalfRLE_tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using Half_RLE;
+
+namespace HalfRLE_tests
+{
+    [TestClass]
+    public class HalfRLE_tests
+    {
+        [TestMethod]
+        public void GetBytes_0x32EC1andGetBytes_rom_GetBytes_result()
+        {
+            int offsetText = 0x32EC1;
+            int offsetCountLines = 0x30946;
+            string pathToROM = @"D:\Repositories\NewCrystal\crystal-plugin-halfrle\HalfRLE_tests\tests\GetBytes_rom";
+            string pathToResult = @"D:\Repositories\NewCrystal\crystal-plugin-halfrle\HalfRLE_tests\tests\GetBytes_result";
+            List<byte> expected = new List<byte>();
+            using (BinaryReader file = new BinaryReader(File.Open(pathToResult, FileMode.Open)))
+            {
+                while(file.BaseStream.Position < file.BaseStream.Length)
+                {
+                    expected.Add(file.ReadByte());
+                }
+            }
+
+            HalfRLE c = new HalfRLE(offsetCountLines.ToString());
+            List<byte> actual = c.GetBytes(offsetText, pathToROM);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ HalfRLE_tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HalfRLE_tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HalfRLE_tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c481bb75-f381-43d8-a1c5-51a3e50daf28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
