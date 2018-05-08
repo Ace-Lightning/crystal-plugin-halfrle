4ed66266083d463fa522020ec747b38b0d267706
Sergey/ka <acelightnin9@gmail.com>
2018-05-08 20:13:58 +0300

    Added test for Insert Bytes method

--- HalfRLE_tests/HalfRLE_tests.cs
+++ HalfRLE_tests/HalfRLE_tests.cs
@@ -10,7 +10,7 @@ namespace HalfRLE_tests
     public class HalfRLE_tests
     {
         [TestMethod]
-        public void GetBytes_0x32EC1andGetBytes_rom_GetBytes_result()
+        public void GetBytes()
         {
             int offsetText = 0x32EC1;
             int offsetCountLines = 0x30946;
@@ -28,7 +28,48 @@ namespace HalfRLE_tests
             HalfRLE c = new HalfRLE(offsetCountLines.ToString());
             List<byte> actual = c.GetBytes(offsetText, pathToROM);
 
-            CollectionAssert.AreEqual(expected, actual);
+            CollectionAssert.AreEqual(expected, actual, "Expected sequence of the bytes is not equal to actual sequence");
+        }
+
+        [TestMethod] 
+        public void InsertBytes()
+        {
+            int offsetText = 0x32EC1;
+            int offsetCountLines = 0x30946;
+            string pathToROM = @"D:\Repositories\NewCrystal\crystal-plugin-halfrle\HalfRLE_tests\tests\InsertBytes_rom";
+            string pathToSequence = @"D:\Repositories\NewCrystal\crystal-plugin-halfrle\HalfRLE_tests\tests\InsertBytes_sequence";
+            List<byte> expectedSequence = new List<byte>();
+            byte expectedCountLines = 0;
+            byte countBytes = 0;
+            using (BinaryReader file = new BinaryReader(File.Open(pathToSequence, FileMode.Open)))
+            {
+                while (file.BaseStream.Position < file.BaseStream.Length)
+                {
+                    countBytes = file.ReadByte();
+                    expectedCountLines++;
+                    expectedSequence.Add(countBytes);
+                    for (int i = 0; i < countBytes; i++)
+                    {
+                        expectedSequence.Add(file.ReadByte());
+                    }
+                }
+            }
+
+            HalfRLE c = new HalfRLE(offsetCountLines.ToString());
+            c.InsertBytes(offsetText, pathToROM, expectedSequence);
+
+            byte actualCounLines = 0;
+
+            using (BinaryReader file = new BinaryReader(File.Open(pathToROM, FileMode.Open)))
+            {
+                file.BaseStream.Seek(offsetCountLines, SeekOrigin.Begin);
+                actualCounLines = file.ReadByte();
+            }
+
+            List<byte> actualSequence = c.GetBytes(offsetText, pathToROM);
+
+            Assert.AreEqual(expectedCountLines, actualCounLines);
+            CollectionAssert.AreEqual(expectedSequence, actualSequence);
         }
     }
 }
